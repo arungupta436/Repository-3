97ab055dd3ff025c2c40f22629f0571f404219e1
arungupta436 <arun.gupta436@gmail.com>
2018-08-12 14:31:03 +0530

    Local to remote

---
+++ Project-3/src/com/project3/Manager.java
@@ -0,0 +1,10 @@
+package com.project3;
+public class Manager {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My  Nama is Sanam");
+
+	}
+
+}
